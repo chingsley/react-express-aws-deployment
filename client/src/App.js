688d976dd28f7f72d56bb9864d18c1ebee06582f
chingsley <kingsley.eneja@getir.com>
2021-05-13 11:55:28 +0100

    create schema sql file

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,29 @@ import React, { Component } from "react";
 import "./App.css";
 import axios from "axios";
 
+const axiosInstance = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com",
+});
+
 class App extends Component {
   state = {
     hello: null,
   };
 
   componentDidMount() {
-    axios
-      .get("/hello")
-      .then((res) => this.setState({ hello: res.data }))
-      .catch((err) => console.log(err));
+    this.asyncFunction();
+    // axiosInstance
+    //   .get("/posts")
+    //   // .then((res) => this.setState({ hello: res.data }))
+    //   .then((res) => console.log(res.data))
+    //   .catch((err) => console.log(err));
   }
+
+  asyncFunction = async () => {
+    await fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+  };
   render() {
     return <div>{this.state.hello ? <div>{this.state.hello}</div> : null}</div>;
   }
