c5df0352c150b13786e6c47e2989f675d4907720
chingsley <kingsley.eneja@getir.com>
2021-05-15 15:56:36 +0100

    add starter code

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,15 @@
 import React, { Component } from "react";
+import Routes from "./routes";
 
-import "./App.css";
-import axios from "axios";
-
-const axiosInstance = axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com",
-});
-
+//main app
 class App extends Component {
-  state = {
-    hello: null,
-  };
-
-  componentDidMount() {
-    this.asyncFunction();
-    // axiosInstance
-    //   .get("/posts")
-    //   // .then((res) => this.setState({ hello: res.data }))
-    //   .then((res) => console.log(res.data))
-    //   .catch((err) => console.log(err));
-  }
-
-  asyncFunction = async () => {
-    await fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((json) => console.log(json));
-  };
   render() {
-    return <div>{this.state.hello ? <div>{this.state.hello}</div> : null}</div>;
+    return (
+      <div>
+        React
+        <Routes />
+      </div>
+    );
   }
 }
 
---
+++ client/src/functional/unauthredirect.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const UnauthRedirect = (props) => <div>UnauthRedirect</div>;
+
+export default UnauthRedirect;
