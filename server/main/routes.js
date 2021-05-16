9cc4cf7464744132691ea99670117857848e3cfc
chingsley <kingsley.eneja@getir.com>
2021-05-16 14:07:36 +0100

    setup comment routes and queries

--- server/main/routes.js
+++ server/main/routes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const pool = require("./db");
 
+/**
+ * POSTS ROUTES SECTION
+ */
 router.get("/api/get/allposts", (req, res, next) => {
   pool.query(
     "SELECT * FROM posts ORDER BY date_created DESC",
@@ -65,7 +68,7 @@ router.delete("/api/delete/postcomments", (req, res, next) => {
 router.delete("/api/delete/post", (req, res, next) => {
   const post_id = req.body.post_id;
   pool.query(
-    `DELETE FROM comments
+    `DELETE FROM posts
               WHERE pid = $1`,
     [post_id],
     (q_err, q_res) => {
@@ -75,4 +78,69 @@ router.delete("/api/delete/post", (req, res, next) => {
   );
 });
 
+/**
+ * COMMENTS ROUTES SECTION
+ */
+
+router.post("/api/post/commenttodb", (req, res, next) => {
+  const values = [
+    req.body.comment,
+    req.body.user_id,
+    req.body.username,
+    req.body.post_id,
+  ];
+  pool.query(
+    `INSERT INTO comments(comment, user_id, author, post_id, date_created)
+              values($1, $2, $3, $4, NOW())`,
+    values,
+    (q_err, q_res) => {
+      if (q_err) return next(q_err);
+      res.json(q_res.rows);
+    }
+  );
+});
+
+router.put("/api/put/commenttodb", (req, res, next) => {
+  const values = [
+    req.body.comment,
+    req.body.user_id,
+    req.body.post_id,
+    req.body.username,
+    req.body.cid,
+  ];
+  pool.query(
+    `UPDATE comments SET comment=$1, user_id=$2, post_id=$3, author=$4, date_created=NOW()
+              WHERE cid=$5`,
+    values,
+    (q_err, q_res) => {
+      if (q_err) return next(q_err);
+      res.json(q_res.rows);
+    }
+  );
+});
+
+router.delete("/api/delete/comment", (req, res, next) => {
+  const cid = req.body.cid;
+  pool.query(
+    `DELETE FROM comments
+              WHERE cid = $1`,
+    [cid],
+    (q_err, q_res) => {
+      if (q_err) return next(q_err);
+      res.json(q_res.rows);
+    }
+  );
+});
+
+router.get("/api/get/allpostcomments", (req, res, next) => {
+  const post_id = String(req.query.post_id);
+  pool.query(
+    `SELECT * FROM comments WHERE post_id = $1`,
+    [post_id],
+    (q_err, q_res) => {
+      if (q_err) return next(q_err);
+      res.json(q_res.rows);
+    }
+  );
+});
 module.exports = router;
